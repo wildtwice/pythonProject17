818526129c06fc6ce30cb1fbbfa5b6586069f5d4
pjb75 <pjb7525@gmail.com>
2022-02-26 16:00:09 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+# a = ['바나나','딸기','포도','사과','멜론','복숭아']
+# for i in range(1,4):
+#     print(a[i])
+
+# b = input("연도를 입력하세요.")
+# if b%4==0 or b%400==0:
+#     print("1")
+# else:
+#     print("0")
+
+# c = [[0,1,2,3]
+#      [4,5,6,7]
+#      [8,9,10,11]]
+#
+# for i in range(2):
+#     for j in range(3):
+#         print(c[i][j])
+
+# d = 10
+# while d<50:
+#     d+=10
+#     print(d)
+
+# e = 0
+# while e < 3:
+#     e += 1
+#     print(f"{e}꼬마")
+# print("인디언")
+
+while True:
+    player = int(input("1을 입력하여 게임을 종료하세요."))
+    if player == 1:
+        print("끝")
+        break
+
+
+
+
+
